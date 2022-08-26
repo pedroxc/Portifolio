1e8c7f1db0eca6d13b0892ad826b7493026d77ca
Pedro <pedroxaviercoelho@gmail.com>
2022-08-26 15:41:48 -0300

    mobileView

--- src/Styles/Global/styles.js
+++ src/Styles/Global/styles.js
@@ -9,17 +9,6 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 36px;
     line-height: 44px;
   }
-  h1{
-    color: #C9C2F2;
-    font-size: 36px;
-  }
-  h2{font-family: Inter;
-font-size: 36px;
-font-weight: 400;
-line-height: 44px;
-letter-spacing: 0em;
-text-align: left;
-}
 button:hover {opacity: 0.7}
 button:active {
   box-shadow: 0 5px #666;
