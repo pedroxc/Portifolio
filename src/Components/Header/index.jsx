0fea15f0282c3f2a5ef8985038868f9cac1dbfa9
Pedro <pedroxaviercoelho@gmail.com>
2022-08-26 15:03:41 -0300

    mobileView

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import icon from "../../Assets/Images/icon.png";
 import { useNavigate } from "react-router-dom";
+import { BrowserView, MobileView } from "react-device-detect";
 
 import { Container, Icon, Button } from "./styles";
 
@@ -8,16 +9,23 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <Icon alt="icon" src={icon} />
-      <Button
-        onClick={() => {
-          navigate("/Certificados");
-        }}
-      >
-        Entre em Contato
-      </Button>
-    </Container>
+    <>
+      <BrowserView>
+        <Container>
+          <Icon alt="icon" src={icon} />
+          <Button
+            onClick={() => {
+              navigate("/Certificados");
+            }}
+          >
+            Entre em Contato
+          </Button>
+        </Container>
+      </BrowserView>
+      <MobileView>
+        <h1>This is rendered only on mobile</h1>
+      </MobileView>
+    </>
   );
 }
 
